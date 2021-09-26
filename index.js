3af27ae3bde7368fe9de89fcaa0500370bf7fd52
mzseaun <69946326+Olu-G@users.noreply.github.com>
2021-09-26 20:04:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Seaun,Gbadebo");
+console.log('hello + world')
+console.log ("hello world")
+console.log(13 + 456)
+console.log('quote the "raven"')
+console.log(2001)
+console.log("waht","do","commas")
+console.log("launch code was founded in 2003")
+console.log("python\nJacascript")
+console.log("python\tJavascript")
+ 
+ let day = "Thursday";
+ day = 32.5;
+ day = 19;
+ console.log(day);
+ const input = require('readline-sync')
+ let name = input.question('enter your name: ')
+ console.log('Hello, ' + Seun + '!');
+ 
